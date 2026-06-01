ce55bcaa5f84ba16719380709166050c7b5996ba
Muhammad Hassan Shafiq <shafiqhassan429@gmail.com>
2026-06-02 01:52:41 +0500

    google tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,16 +132,15 @@ function RootShell({ children }: { children: ReactNode }) {
   }}
 />
         <HeadContent />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1176024467991038');fbq('track', 'PageView');`,
-          }}
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1176024467991038&ev=PageView&noscript=1" />`,
-          }}
-        />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3EDYKTZ0JJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3EDYKTZ0JJ');
+</script>
       </head>
       <body>
         {children}
