0fd836536c55790bad0bf27a0aa5ac3434ee00b5
Muhammad Hassan Shafiq <shafiqhassan429@gmail.com>
2026-06-02 01:45:16 +0500

    google analytics integration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -107,6 +107,21 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-3EDYKTZ0JJ"
+/>
+
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-3EDYKTZ0JJ');
+    `,
+  }}
+/>
         <HeadContent />
         <script
           dangerouslySetInnerHTML={{
