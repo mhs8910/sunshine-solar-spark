c40ffb28d819a2b7aa16f59ebc10c0c63b04faec
Muhammad Hassan Shafiq <shafiqhassan429@gmail.com>
2026-06-03 01:00:22 +0500

    google tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,14 +132,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     scripts: [
       {
         async: true,
-        src: "https://www.googletagmanager.com/gtag/js?id=G-3EDYKTZ0JJ",
+        src: "https://www.googletagmanager.com/gtag/js?id=G-675C2BYVN7",
       },
       {
         children: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-3EDYKTZ0JJ');
+          gtag('config', 'G-675C2BYVN7');
         `,
       },
     ],
